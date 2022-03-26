55e9f1b06d23d73e97e9e891880f5034a0410baf
Mohammadreza Ziraki <mohammadreza.ziraki@gmail.com>
2022-03-26 20:47:24 +0430

    Add default sever url for swagger

--- data-app/src/main/java/net/muon/data/app/MuonDataApplication.java
+++ data-app/src/main/java/net/muon/data/app/MuonDataApplication.java
@@ -1,9 +1,12 @@
 package net.muon.data.app;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 @EnableScheduling
 public class MuonDataApplication
